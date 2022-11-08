1ff63b1f6fd54c17e9d9222b9c6a24d7e278b97a
Fillinius <113914362+Fillinius@users.noreply.github.com>
2022-11-08 12:22:09 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+// "JavaScript" - код с созданием переменных
+
+/* "Автор:"
+Владимир Ревягин */
+
+const myName = 'Vladimir'
+const mySurName = ' Revyagin'
+let myFavoriteDrink = 'water'
+const myFavoriteAnimal = 'Cats'
+const myFavoriteProgrammingLanguage ='JavaScript'
+
+console.log(myName + mySurName)
+console.log(myFavoriteDrink)
+console.log(myFavoriteAnimal)
+console.log(myFavoriteProgrammingLanguage);
